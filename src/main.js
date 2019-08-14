b341eb6858817b0b43d3177b85dee3297b417a77
赵采霞 <121357625@qq.com>
2019-08-14 10:16:02 +0800

    增加了axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './css/index.css'
-
 import Vue from 'vue'
 import app from './App.vue'
 
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 import router from './router.js'
+
+import axios from 'axios'
+Vue.prototype.$axios = axios
 
 var vm = new Vue({
     el:'#app',
